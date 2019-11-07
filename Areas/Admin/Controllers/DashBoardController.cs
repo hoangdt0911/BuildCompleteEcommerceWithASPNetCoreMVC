cffb4b5cf4d775060472b5c7980c1eb1810612f6
Hoang <Hoang@DESKTOP-866DMLA>
2019-11-08 00:53:29 +0700

    test 1

---
+++ Areas/Admin/Controllers/DashBoardController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuildCompleteEcommerceWithASPNetCoreMVC.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("admin/dashboard")]
+    public class DashBoardController : Controller
+    {
+        [Route("")]
+        [Route("index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
